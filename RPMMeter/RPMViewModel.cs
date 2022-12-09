3a149dc52e550c6c1069cfc79cf4b04475244adb
MladejLeopard <jakub.duch@gmail.com>
2022-12-09 18:09:13 +0100

    Adding Rpm meter

---
+++ RPMMeter/RPMViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPMMeter
+{
+    class RPMViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChange(string info)
+        {
+            if(PropertyChanged!= null) 
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+
+      
+        int _angle;
+        public int Angle
+        {
+            get
+            {
+                return _angle;
+            }
+            set
+            {
+                _angle = value;
+                NotifyPropertyChange("Angle");
+            }
+        }
+        int _value;
+        public int Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                if(value >=0 && value <= 9000)
+                {
+                    _value = value;
+                    Angle = value - 85;
+                    NotifyPropertyChange("Angle");
+                }
+            }
+        }
+    }
+}
---
+++ RPMMeter/RPMs.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RPMMeter
+{
+    /// <summary>
+    /// Interaction logic for RPMs.xaml
+    /// </summary>
+    public partial class RPMs : UserControl
+    {
+        public RPMs()
+        {
+            InitializeComponent();
+            this.DataContext = new RPMViewModel();
+        }
+    }
+}
