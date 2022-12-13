30aae016f007277a571889ad330a65c6758d49ed
MladejLeopard <jakub.duch@gmail.com>
2022-12-13 17:46:02 +0100

    Added Padding in RPM.xaml Added StartEngine class Added method templates in StartEngine.cs

---
+++ RPMMeter/StartEngine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPMMeter
+{
+    internal abstract class StartEngine
+    {
+        int _force;
+        public void GetInitialForce()
+        {
+
+        }
+
+        public void GetInitialTorque() 
+        {
+
+        }
+        public void GetInitialRpm()
+        {
+
+        }
+    }
+}
