a32842999d0c64f36f6960984cf59a206c91f7b2
MladejLeopard <jakub.duch@gmail.com>
2023-01-12 14:33:12 +0100

    Testing Data Binding and NotifyPropertyChange

--- RPMMeter/ViewModels/TestViewModel.cs
+++ RPMMeter/ViewModels/TestViewModel.cs
@@ -9,5 +9,54 @@ namespace RpmMeter.ViewModels
 {
     public class TestViewModel : Screen
     {
-    }
+		private int _testingRpm = 1200;
+        private int _testingSpeed;
+		private int _testingForce;
+
+
+        public int TestingRpm
+		{
+			get 
+			{
+				return _testingRpm;
+			}
+			set 
+			{
+				_testingRpm = value;
+				NotifyOfPropertyChange(() => TestingRpm);
+			}
+		}
+
+		public int TestingSpeed
+		{
+			get 
+			{
+				return _testingSpeed;
+			}
+			set
+			{
+				_testingSpeed = value;
+				NotifyOfPropertyChange(() => TestingSpeed);
+			}
+		}
+
+
+		public int TestingForce
+		{
+			get
+			{
+				return _testingForce;
+			}
+
+			set 
+			{
+				_testingForce = value; 
+			}
+		}
+
+
+
+
+
+	}
 }
